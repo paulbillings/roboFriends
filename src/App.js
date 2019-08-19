f59073f950b02114dc0d4b6bc0b16705aaf046eb
Paul Billings <paulbillings101@yahoo.co.uk>
2019-08-19 19:24:02 +0100

    Added robot json connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 
@@ -9,9 +8,15 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ""
 		}
+	}
+
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users }))
 	}
 
 	onSearchChange = (event) => {
@@ -22,13 +27,18 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return (
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots} />
-			</div>
-		);	
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		} else {
+			return (
+				<div className="tc">
+					<h1 className="f1">RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={filteredRobots} />
+				</div>
+			);	
+		}
+		
 	}
 	
 }
